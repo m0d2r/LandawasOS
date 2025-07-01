a76c72e76b27901d1cb51ae76a73e4952a0aee23
m0d2r <mineceafjozikrorokcz@gmail.com>
2025-07-01 13:56:08 +0000

    V0.02.3

--- start.lua
+++ start.lua
@@ -2,6 +2,14 @@ term.clear()
 term.setCursorPos(1,1)
 disk.setLabel("disk", "Landawas Operating System")
 
+-- Startup update
+shell.run("delete startup.lua")
+shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/startup.lua startup.lua")
+if sucess them
+    shell.run("startup.lua")
+else:
+    print("Failed to update AutoUpdater")
+
 -- OS info
 term.setTextColour(colors.lime)
 term.write("LandawasOS V0.02.2")
--- startup.lua
+++ startup.lua
@@ -3,6 +3,9 @@ print("Installing Updates...")
 
 -- StartUp update
 shell.run("delete start.lua")
-shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/start. start.lua")¨
-print("Start.lua has been updated")
-shell.run("start.lua")
+local sucess shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/start. start.lua")
+if sucess them
+    print("Start.lua has been updated")
+    shell.run("start.lua")
+else:
+    print("Failed to update start.lua")
