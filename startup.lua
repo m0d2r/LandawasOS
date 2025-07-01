9c58237002ee6d7a8b7271992ddc8aacbaa66190
m0d2r <mineceafjozikrorokcz@gmail.com>
2025-07-01 13:15:09 +0200

    Landawas V0.01.1
    Signed-off-by: m0d2r <mineceafjozikrorokcz@gmail.com>

--- startup.lua
+++ startup.lua
@@ -2,11 +2,12 @@ term.clear()
 term.setCursorPos(1,1)
 disk.setLabel("disk", "Landawas Operating System")
 
+textutils.slowPrint("---------------------------------------------------")
 term.setTextColour(colors.lime)
 term.write("LandawasOS")
 term.setTextColour(colors.white)
 print(" By StormPatrik")
 
 print("Thank you for chosing LandawasOS")
-print("Beta: V0.01")
+print("Beta: V0.01.1")
 textutils.slowPrint("---------------------------------------------------")
