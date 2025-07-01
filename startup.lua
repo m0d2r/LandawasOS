aff6a0e7cc200b1cd014358baf140475c8a47b87
m0d2r <mineceafjozikrorokcz@gmail.com>
2025-07-01 14:04:09 +0000

    V0.02.7

--- startup.lua
+++ startup.lua
@@ -4,7 +4,7 @@ print("Installing Updates...")
 -- StartUp update
 shell.run("delete start.lua")
 local sucess shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/start.lua start.lua")
-if sucess them
+if sucess then
     print("Start.lua has been updated")
     shell.run("start.lua")
 else:
