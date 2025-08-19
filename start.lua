26d1891a7581177a35949d242a73b99da6b4cfd7
m0d2r <mineceafjozikrorokcz@gmail.com>
2025-08-19 00:45:17 +0000

    V0.5.1

--- start.lua
+++ start.lua
@@ -2,15 +2,18 @@ term.clear()
 term.setCursorPos(1,1)
 disk.setLabel("disk", "Landawas Operating System")
 
--- OS info
-term.setTextColour(colors.lime)
-term.write("LandawasOS V0.05")
-term.setTextColour(colors.white)
-print(" by: m0d3r")
-
 --boot info
 term.write("Loading system ")
 textutils.slowPrint("......")
 
 print("")
 print("Warning: Beta versions may be unstable and buggy")
+sleep(3)
+
+-- system info
+term.clear()
+term.setCursorPos(1,1)
+term.setTextColour(colors.lime)
+term.write("LandawasOS V0.05.1")
+term.setTextColour(colors.white)
+print(" by: m0d3r")
--- update.lua
+++ update.lua
@@ -1,11 +1,16 @@
 term.clear()
 term.setCursorPos(1,1)
 
--- Updater
+-- Updater versuin
+print("Updater V1.0.0")
+
+-- Updater info
 print("Deleting old files...")
 shell.run("delete start.lua")
 shell.run("delete startup.lua")
 shell.run("delete Programs.lua")
+
+print("Downloading new files...")
 shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/start.lua start.lua")
 shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/startup.lua startup.lua")
 shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/Programs.lua Programs.lua")
@@ -15,6 +20,9 @@ print("Old files has been deleted successfully.")
 print("Start.lua has been downloaded")
 print("Startup.lua has been downloaded")
 print("Programs.lua has been downloaded")
+
+print("All programs and system modules has been downloaded")
+
 print("Starting start script...")
 
 sleep(1)
