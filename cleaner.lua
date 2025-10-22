2ff7c7c63a2f76ae5f59af0b8989bad665e06950
m0d2r <mineceafjozikrorokcz@gmail.com>
2025-10-22 15:50:24 +0000

    V0.5.1.3 B5

--- cleaner.lua
+++ cleaner.lua
@@ -2,6 +2,8 @@ term.clear()
 term.setCursorPos(1,1)
 
 -- cůear
+textutils.slowPrint("---------------------------------------------------")
 print("Cleaming...")
 shell.rum("delete Update.lua")
 print("1 file/s deleted.")
+textutils.slowPrint("---------------------------------------------------")
