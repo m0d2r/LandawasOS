646a645f3e81d0a0b48679503f06d1aa30f23c2e
m0d2r <mineceafjozikrorokcz@gmail.com>
2025-10-22 16:22:00 +0000

    V0.5.1.3.1.1

--- update.lua
+++ update.lua
@@ -18,7 +18,7 @@ if ans == "y" then
     -- Files download
     shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/start.lua start.lua")
     shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/startup.lua startup.lua")
-    shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/Programs.lua programs.lua")
+    shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/programs.lua programs.lua")
     shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/cleaner.lua cleaner.lua")
 
     print("Starting start script...")
