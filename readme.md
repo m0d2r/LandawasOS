5ebba3fa9bb199ee84dd2114ecd77cb1a58b4eba
m0d2r <mineceafjozikrorokcz@gmail.com>
2025-10-22 16:29:34 +0000

    V0.5.1.3.3

--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@ Features:
 # How to install
 1. Run lua in the terminal
 2. them run this:
-shell.run("delete update.lua"); shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/update.lua update.lua"); shell.run("update.lua")
+shell.run("delete startup.lua"); shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/startup.lua startup.lua"); shell.run("startup.lua")
 3. done
 
 This will install LandawasOS to the computer
--- startup.lua
+++ startup.lua
@@ -1,3 +1,26 @@
-shell.run("dekete update.lua")
+term.clear()
+term.setCursorPos(1,1)
+
+-- install update
+shell.run("dekete startup.lua")
 shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/update.lua update.lua")
-shell.run("start.lua")
+
+-- boot info
+term.write("Loading system ")
+textutils.slowPrint("......")
+
+-- info
+print("")
+print("Warning: Beta versions may be unstable and buggy")
+sleep(2)
+
+-- system info
+term.clear()
+term.setCursorPos(1,1)
+term.setTextColour(colors.lime)
+term.write("LandawasOS V0.05.1.3.1")
+term.setTextColour(colors.white)
+print(" by: m0d3r")
+
+-- Label
+os.setComputerLabel("LandawasOS")
