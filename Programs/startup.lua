40267ccb2751eb7f1526da2dfd137f1d44128167
m0d2r <mineceafjozikrorokcz@gmail.com>
2026-01-15 13:54:52 +0100

    V0.6.1
    Signed-off-by: m0d2r <mineceafjozikrorokcz@gmail.com>

--- Programs/startup.lua
+++ Programs/startup.lua
@@ -2,6 +2,7 @@ term.clear()
 term.setCursorPos(1,1)
 
 -- install update
+Print("Updating update.lua")
 shell.run("delete update.lua")
 shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/Programs/update.lua update.lua")
 
@@ -19,7 +20,7 @@ sleep(2)
 term.clear()
 term.setCursorPos(1,1)
 term.setTextColour(colors.lime)
-term.write("LandawasOS V0.6 Hotfix 10")
+term.write("LandawasOS V0.6.1")
 term.setTextColour(colors.white)
 print(" by: m0d3r")
 
