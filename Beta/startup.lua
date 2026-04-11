f8a3d954896839ce9f517a1bcdbca4e571a73c15
m0d2r <mineceafjozikrorokcz@gmail.com>
2026-04-11 23:53:13 +0200

    Build 2
    GUI startup

--- Beta/startup.lua
+++ Beta/startup.lua
@@ -2,20 +2,33 @@
 local orange = colors.orange
 local white = colors.white
 local green = colors.green
-local version = "BUILD: 1"
+local version = "Build: 2"
 local autor = "By m0d2r"
-
--- Beggining
-term.clear()
-term.setCursorPos(1,1)
 
 -- install update
 print("Updating update.lua")
 shell.run("delete update.lua")
 shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/Programs/update.lua update.lua")
 
+-- Start up
+term.setTextColor(colors.orange)
+term.setBackgroundColor(colors.gray)
 term.clear()
+
+-- Text
 term.setCursorPos(1,1)
+print("---------------------------------------------------")
+term.setTextColour(colors.lime)
+print("LandaWasOS GUI")
+print("V0.0.3")
+print("by: m0d2r")
+term.setTextColor(colors.orange)
+term.setCursorPos(1,1)
+print("You can use progrmas to show list of programs")
+term.setCursorPos(1,18)
+print("---------------------------------------------------")
+sleep(2)
+term.setCursorPos(1,2)
 
 -- Text
 term.write("Starting services")
@@ -28,68 +41,8 @@ term.write("Loading system information")
 textutils.slowPrint("..")
 term.write("Loading commands")
 textutils.slowPrint("...")
+term.write("Loading GUI")
+textutils.slowPrint(".............")
 
-print("")
-print("boot completed...")
-
--- clean
-sleep(1)
-term.clear()
-term.setCursorPos(1,1)
-
--- OS info
-term.setTextColor(green)
-term.write(version)
-term.setTextColor(orange)
-term.setCursorPos(1,2)
-term.write(autor)
-
--- Information
-term.setCursorPos(1,4)
-term.setTextColor(white)
-print("Type programs for all programs")
-
--- program loop
-term.setCursorPos(1,6)
-
-while true do
-    -- User
-    term.setTextColor(green)
-    write("LandaWasOS")
-    term.setTextColor(white)
-    write(" > ")
-    
-    local input = read()
-    
-    -- Build in commands
-    if input == "exit" then
-        break
-
-    elseif input == "fetch" then
-        write("OS: ")
-        term.setTextColor(green)
-        print("LandaWasOS")
-        term.setTextColor(white)
-        write("OS Version: ")
-        term.setTextColor(green)
-        print(version)
-        print("autor", autor)
-        term.setTextColor(white)
-        sleep(1)
-        
-    elseif input == "reboot" then
-        term.setTextColor(orange)
-        print("Rebooting...")
-        sleep(1)
-        os.reboot()
-    
-    elseif input == "shutdown" then
-        tern.setTextColor(orange)
-        print("Shutting down...")
-        sleep(1)
-        os.shutdown()
-    
-    else
-        shell.run(input)
-    end
-end
+-- Load GUI
+print("GUI is comming soon...")
