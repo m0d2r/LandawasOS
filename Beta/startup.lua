8fdffd37be0442a399a1754fa7d0eecac3b7beb0
m0d2r <mineceafjozikrorokcz@gmail.com>
2026-04-12 00:21:26 +0200

    Build 5

--- Beta/startup.lua
+++ Beta/startup.lua
@@ -2,8 +2,11 @@
 local orange = colors.orange
 local white = colors.white
 local green = colors.green
-local version = "Build: 4"
+local gray = colors.gray
+local lime = colors.lime
+local version = "Build: 5"
 local autor = "By m0d2r"
+local line = "---------------------------------------------------"
 
 -- install update
 print("Updating update.lua")
@@ -11,24 +14,32 @@ shell.run("delete update.lua")
 shell.run("wget https://raw.githubusercontent.com/m0d2r/LandawasOS/main/Programs/update.lua update.lua")
 
 -- Start up
-term.setTextColor(colors.orange)
-term.setBackgroundColor(colors.gray)
+term.setTextColor(orange)
+term.setBackgroundColor(gray)
 term.clear()
 
 -- Text
 term.setCursorPos(1,1)
-print("---------------------------------------------------")
-term.setTextColour(colors.lime)
+print(line)
+term.setTextColour(lime)
 print("LandaWasOS GUI")
 print(version)
 print(autor)
-term.setTextColor(colors.orange)
+term.setTextColor(orange)
 term.setCursorPos(1,18)
-print("---------------------------------------------------")
+print(line)
 sleep(2)
-term.setCursorPos(1,2)
+term.clear()
 
 -- Text
+term.setCursorPos(1,1)
+print(line)
+term.setCursorPos(1,18)
+print(line)
+term.setCursorPos(1,2)
+term.setTextColor(white)
+
+-- Status
 term.write("Starting services")
 textutils.slowPrint("........")
 term.write("Starting System")
@@ -40,8 +51,9 @@ textutils.slowPrint("..")
 term.write("Loading commands")
 textutils.slowPrint("...")
 term.write("Loading GUI")
-textutils.slowPrint(".............")
-term.setCursorPos(1,8)
+textutils.slowPrint("....")
+
+term.setCursorPos(1,9)
 print("You can use progrmas to show list of programs")
 
 -- program loop
